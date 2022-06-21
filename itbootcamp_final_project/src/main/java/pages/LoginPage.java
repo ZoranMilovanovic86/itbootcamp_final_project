b8459831d6fae928fa117486a9b88578bb9d1f33
Zoran Milovanovic <mr.zoranmilovanovic@gmail.com>
2022-06-21 10:41:27 +0200

    Login page input types checks and updated LoginPage with necessary methods

--- itbootcamp_final_project/src/main/java/pages/LoginPage.java
+++ itbootcamp_final_project/src/main/java/pages/LoginPage.java
@@ -1,12 +1,22 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginPage {
     private WebDriver driver;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public WebElement getEmailInputField() {
+        return this.driver.findElement(By.id("email"));
+    }
+
+    public WebElement getPasswordInputField() {
+        return this.driver.findElement(By.id("password"));
     }
 
 }
--- itbootcamp_final_project/src/main/java/tests/LoginTests.java
+++ itbootcamp_final_project/src/main/java/tests/LoginTests.java
@@ -5,12 +5,19 @@ import org.testng.annotations.Test;
 
 public class LoginTests extends BasicTest {
 
-    @Test
-    public void VisitLoginPage() {
+    @Test(priority = 10)
+    public void visitLoginPage() {
         navPage.getLanguageButtonEN().click();
         navPage.getLoginButton().click();
         Assert.assertTrue(driver.getCurrentUrl().contains("/login"),
                 "Current site path do not contains /login in URL");
+    }
+
+    @Test(priority = 20)
+    public void verifyInputTypes() {
+        navPage.getLoginButton().click();
+        Assert.assertEquals(loginPage.getEmailInputField().getAttribute("type"), "email",
+                "Email input field attribute type is not email");
     }
 
 }
