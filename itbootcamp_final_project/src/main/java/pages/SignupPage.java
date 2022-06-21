7306e5c73d6572aae57efaa92393ef731764bb7c
Zoran Milovanovic <mr.zoranmilovanovic@gmail.com>
2022-06-21 14:59:47 +0200

    Signup page input type checks tests

--- itbootcamp_final_project/src/main/java/pages/SignupPage.java
+++ itbootcamp_final_project/src/main/java/pages/SignupPage.java
@@ -1,6 +1,8 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SignupPage {
     private WebDriver driver;
@@ -8,4 +10,20 @@ public class SignupPage {
     public SignupPage(WebDriver driver) {
         this.driver = driver;
     }
+    public WebElement getNameInputField() {
+        return this.driver.findElement(By.id("name"));
+    }
+
+    public WebElement getEmailInputField() {
+        return this.driver.findElement(By.id("email"));
+    }
+
+    public WebElement getPasswordInputField() {
+        return this.driver.findElement(By.id("password"));
+    }
+
+    public WebElement getConfirmPasswordInputField() {
+        return this.driver.findElement(By.id("confirmPassword"));
+    }
+
 }
--- itbootcamp_final_project/src/main/java/tests/SignupTests.java
+++ itbootcamp_final_project/src/main/java/tests/SignupTests.java
@@ -11,4 +11,15 @@ public class SignupTests extends BasicTest {
         Assert.assertTrue(driver.getCurrentUrl().contains("/signup"),
                 "Current site path do not contains /signup in URL");
     }
+
+    @Test(priority = 20)
+    public void verifyInputTypes() {
+        navPage.getSignupButton().click();
+        Assert.assertEquals(signupPage.getEmailInputField().getAttribute("type"), "email",
+                "Email input field attribute type is not email");
+        Assert.assertEquals(signupPage.getPasswordInputField().getAttribute("type"), "password",
+                "Password input field attribute type is not password");
+        Assert.assertEquals(signupPage.getConfirmPasswordInputField().getAttribute("type"), "password",
+                "Confirm Password input field attribute type is not password");
+    }
 }
