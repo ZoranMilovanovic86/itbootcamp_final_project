eeadb918341edc66ca876e46ca34c05d9b6510b4
Zoran Milovanovic <mr.zoranmilovanovic@gmail.com>
2022-06-23 08:43:03 +0200

    AuthRoutes Tests - Forbids visits to home url if not authenticated

--- itbootcamp_final_project/src/main/java/pages/NavPage.java
+++ itbootcamp_final_project/src/main/java/pages/NavPage.java
@@ -3,6 +3,10 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class NavPage {
     private WebDriver driver;
@@ -35,4 +39,14 @@ public class NavPage {
     public WebElement getCitiesButton() {
         return this.driver.findElement(By.linkText("Cities"));
     }
+
+    public void getHomeLink() {
+        this.driver.navigate().to(driver.getCurrentUrl() + "home");
+    }
+
+    public void waitForLoginPageToLoadUp() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlContains("/login"));
+    }
+
 }
---
+++ itbootcamp_final_project/src/main/java/tests/AuthRoutesTests.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AuthRoutesTests extends BasicTest {
+
+    @Test(priority = 10)
+    public void ForbidsVisitsToHomeUrlIfNotAuthenticated() {
+        navPage.getHomeLink();
+        navPage.waitForLoginPageToLoadUp();
+        Assert.assertTrue(this.driver.getCurrentUrl().contains("/login"),
+                "Current site path do not contains /login in URL");
+    }
+
+}
